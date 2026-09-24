07f634f64444652a1b20c04e256b6e3300ff8ce9
o.guduf@gmail.com <o.guduf@gmail.com>
2026-09-24 16:44:01 -0400

    updating terraform file

--- infra/application/parameters.tf
+++ infra/application/parameters.tf
@@ -3,6 +3,10 @@ resource "aws_ssm_parameter" "frontend_image" {
   type   = "SecureString"
   key_id = aws_kms_key.application.arn
   value  = "not-deployed"
+
+  lifecycle {
+    ignore_changes = [value]
+  }
 }
 
 resource "aws_ssm_parameter" "backend_image" {
@@ -10,4 +14,8 @@ resource "aws_ssm_parameter" "backend_image" {
   type   = "SecureString"
   key_id = aws_kms_key.application.arn
   value  = "not-deployed"
+
+  lifecycle {
+    ignore_changes = [value]
+  }
 }
